a4af252815ac05c725421ab72ff8cf5f1976443c
Erick Briones <erickalan518@gmail.com>
2026-07-18 22:16:02 -0600

    Add movie slideshow feature with custom app bar and datasource integration

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,9 +1,10 @@
+import 'package:dio/dio.dart';
+
 import 'package:cinemapedia/config/constants/environment.dart';
-import 'package:cinemapedia/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
-import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
   final dio = Dio(
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,19 +1,18 @@
-// import 'package:cinemapedia/config/constants/environment.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
-  
   static const name = 'home-screen';
 
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _HomeView()
-    );
+    return Scaffold(body: _HomeView());
   }
 }
 
@@ -25,7 +24,6 @@ class _HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<_HomeView> {
-
   @override
   void initState() {
     super.initState();
@@ -34,19 +32,24 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
-    
-    if (nowPlayingMovies.isEmpty) return CircularProgressIndicator();
-    
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title)
-        );
-      },
+    // if (slideShowMovies.isEmpty) return Center(child: CircularProgressIndicator());
+
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: slideShowMovies)
+        // Expanded(
+        //   child: ListView.builder(
+        //     itemCount: nowPlayingMovies.length,
+        //     itemBuilder: (context, index) {
+        //       final movie = nowPlayingMovies[index];
+        //       return ListTile(title: Text(movie.title));
+        //     },
+        //   ),
+        // ),
+      ],
     );
   }
 }
